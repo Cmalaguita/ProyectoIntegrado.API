40c4b4ca265e951f469c4d10d132acd9eeaf44a8
Cmalaguita <72543773+Cmalaguita@users.noreply.github.com>
2022-03-05 00:25:03 +0100

    cambio en la interfaz del repositorio de facturas

--- ProyectoIntegrado.DAL/Repositories/Contracts/IFacturaRepository.cs
+++ ProyectoIntegrado.DAL/Repositories/Contracts/IFacturaRepository.cs
@@ -8,7 +8,7 @@ namespace ProyectoIntegrado.DAL.Repositories.Contracts
    public interface IFacturaRepository
     {
         List<Factura> ObtenerFacturasPorEmpresaId(int empresaId);
-        bool ObtenerFacturaPagadaMasReciente(int empresaId);
+        Factura ObtenerFacturaPagadaMasReciente(int empresaId);
         List<Factura> ObtenerFacturasPorContratoId(int contratoId);
         List<Factura> ObtenerFacturasPorSuscripcionId(string suscripcionId);
         List<Factura> ObtenerFacturasPorEmpresaYContratoId(int empresaId,int contratoId);
